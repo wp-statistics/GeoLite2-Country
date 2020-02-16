ec78a29e0ccc1b23972cd1a094a6d755124c47d5
Mostafa Soufi <mostafa.soufi@hotmail.com>
2020-02-16 12:02:59 +0200

    Update the GeoIP to 20200211

--- update.sh
+++
@@ -1,5 +0,0 @@
-rm -rf GeoLite2-Country.mmdb.gz
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz GeoLite2-Country.mmdb.gz
-git add .
-git commit -m "The database updated!"
-git push
